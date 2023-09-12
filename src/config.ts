f57f0f28deca0b0e06a53317dcb5c234c26a8f71
Zach <zmriel@gmail.com>
2023-09-12 13:55:19 -0400

    chore(deps)!: update all dependencies

--- src/config.ts
+++ src/config.ts
@@ -4,11 +4,11 @@ import type { FlatConfig } from "./flat-eslint-config.js";
 import { configs as importConfigs } from "./import.js";
 import { config as typescriptConfig } from "./typescript.js";
 
-const config = [
+const config: FlatConfig = [
   eslintConfig,
   ...importConfigs,
   typescriptConfig,
   prettierConfig,
-] satisfies FlatConfig;
+];
 
 export default config;
